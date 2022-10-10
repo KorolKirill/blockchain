c45427ae73eca870d445d5b667b6c541a07574c1
Korol Kirill <71098249+KorolKirill@users.noreply.github.com>
2022-10-10 16:01:23 +0300

    done

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,91 @@
 import hashlib
 import json
-
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+from flask import Flask, jsonify
+from urllib.parse import urlparse
+import requests
 
 
 class Blockchain(object):
-    def __init__(self):
+
+    def __init__(self, miner):
         self.chain = []
-        self.current_transactions = []
-        self.new_block(previous_hash='korol', proof=19112002)
+        self.mem_pool = []
+        self.nodes = set()
+        self.miner = miner
+        # genesis block
+        self.mineBlock(previous_hash='korol', proof=19112002)
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def calculate_balances(self):
+        balances = {}
+        for i in range(len(self.chain)):
+            for o in range(len(self.chain[i]["transactions"])):
+                trxn = self.chain[i]["transactions"][o]
+                if balances.get(trxn["recipient"]) is None:
+                    balances[trxn["recipient"]] = trxn["amount"]
+                else:
+                    balances[trxn["recipient"]] += trxn["amount"]
+
+                if trxn["sender"] != "0":
+                    balances[trxn["sender"]] -= trxn["amount"]
+        return balances
+
+    def new_transaction(self, sender, recipient, amount):
+        self.mem_pool.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        self.share_mempool()
+
+    def share_mempool(self):
+        neighbours = self.nodes
+        data = {
+            'mem_pool': self.mem_pool
+        }
+        for node in neighbours:
+            response = requests.post(f'http://{node}/refresh_mempool', json=data)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            if not self.valid_proof(last_block):
+                return False
+
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
 
     def proof_of_work(self, block):
         while self.valid_proof(block) is False:
@@ -17,38 +93,60 @@ class Blockchain(object):
         return block
 
     def valid_proof(self, block):
-        guess_hash = Blockchain.hash(block)
-        return guess_hash[-2:] == "11"  # У якості підтвердження доказу - наявність в кінці хешу [місяць народження].
+        # У якості підтвердження доказу - наявність в кінці хешу [місяць народження].
+        return Blockchain.hash(block)[-2:] == "11"
 
     def get_last_proof(self):
-        if self.chain[-1] is None:
-            return 0
-        return self.chain[-1]['proof']
+        if len(self.chain) > 0:
+            return self.chain[-1]['proof']
+        return 0
 
-    def new_block(self, proof=None, previous_hash=None):
+
+    def mineBlock(self, proof=None, previous_hash=None):
+        minerAddress = self.miner
+        # я Coinbase-транзакції
+        minerReward = 19
+        self.resolve_conflicts()
+        coinbase = {
+            'sender': '0',
+            'recipient': minerAddress,
+            'amount': minerReward,
+        }
+        self.new_block(coinbaseTRXN=coinbase, proof=proof, previous_hash=previous_hash)
+
+    def new_block(self, coinbaseTRXN, proof=None, previous_hash=None):
         if proof is None:
             proof = self.get_last_proof()
+
+        trxn_passed = []
+        trxn_passed.append(coinbaseTRXN) # adds coinbase
+        transaction_limit_per_block = 5
+        balances = self.calculate_balances()
+        counter = 0
+        while counter < transaction_limit_per_block and len(self.mem_pool) > 0:
+            upcoming_trxn = self.mem_pool.pop(0)
+            if balances.get(upcoming_trxn['sender']) is not None:
+                if balances[upcoming_trxn['sender']] > upcoming_trxn['amount']:
+                    balances[upcoming_trxn['sender']] -= upcoming_trxn['amount']
+                    if balances.get(upcoming_trxn["recipient"]) is None:
+                        balances[upcoming_trxn["recipient"]] = upcoming_trxn["amount"]
+                    else:
+                        balances[upcoming_trxn["recipient"]] += upcoming_trxn["amount"]
+                    trxn_passed.append(upcoming_trxn)
+                    counter += 1
+                    continue
 
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
-            'transactions': self.current_transactions,
+            'transactions': trxn_passed,
             'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
         block = self.proof_of_work(block)
-
-        self.current_transactions = []
+        self.share_mempool()
         self.chain.append(block)
         return block
-
-    def new_transaction(self, sender, recipient, amount):
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
-        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
--- main.py
+++ main.py
@@ -1,21 +1,143 @@
-# This is a sample Python script.
+from uuid import uuid4
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from flask import Flask, jsonify, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from blockchain import Blockchain
 
-import blockchain;
 
+def lab2():
+    blockchainTest = Blockchain()
+    print(blockchainTest.last_block)
+    blockchainTest.mineBlock()
+    print(blockchainTest.last_block)
+    blockchainTest.new_transaction("Kirill", "oleg", 3)
+    blockchainTest.new_transaction("Kirill", "vanya", 5)
+    blockchainTest.mineBlock()
+    blockchainTest.new_transaction("Kirill", "oleg", 2)
+    print(blockchainTest.last_block)
+    print(blockchainTest.calculate_balances())
+
+def lab1():
+    blockchainTest = Blockchain()
+    print(blockchainTest.last_block)
+    print(blockchainTest.hash(blockchainTest.last_block))
+    blockchainTest.new_transaction("kirill", "alex", 0.3)
+
+    blockchainTest.new_block()
+    print(blockchainTest.last_block)
+    print(blockchainTest.hash(blockchainTest.last_block))
+
+
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-', '')
+blockchain = Blockchain("kirill")
+
+
+@app.route('/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values['nodes']
+    if nodes is None:
+        return {"message": "Error: Please supply a valid list of nodes", "m": nodes}, 300
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/refresh_mempool', methods=['POST'])
+def refresh_mempool():
+    values = request.get_json()
+
+    mem_pool = values.get('mem_pool')
+    if mem_pool is None:
+        return "Error: wrong mem_pool", 400
+    blockchain.mem_pool = mem_pool
+
+    response = {
+        'message': 'mem_pool was updated',
+        'current_mem_pool': mem_pool,
+    }
+    return jsonify(response), 201
+
+
+@app.route('/balances', methods=['GET'])
+def balances():
+    balances = blockchain.calculate_balances()
+    response = {"balances": balances}
+    return jsonify(response), 200
+
+@app.route('/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    blockchain.mineBlock()
+    response = {
+        'message': 'Block was successfully mined',
+        'last block': blockchain.last_block
+    }
+    return jsonify(response), 200
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    # Перевірка того, що необхідні поля знаходяться серед POST-даних
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+        # Створення нової транзакції
+    blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction was added to the mempool and waiting to be added to Block'}
+    return jsonify(response), 201
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    blockchain = blockchain.Blockchain()
+    # lab1()
+    # lab2()
+    blockchain.mineBlock()
     print(blockchain.last_block)
-    print(blockchain.hash(blockchain.last_block))
-    blockchain.new_transaction("kirill", "alex", 0.3)
+    blockchain.new_transaction("kirill", "oleg", 3)
+    blockchain.new_transaction("kirill", "vanya", 5)
+    blockchain.new_transaction("kirill", "oleg", 2)
+    print(blockchain.mem_pool)
+    print(  blockchain.last_block)
+    blockchain.mineBlock()
+    print(blockchain.mem_pool)
+    print(  blockchain.last_block)
+    blockchain.mineBlock()
+    print(  blockchain.last_block)
+    print(  blockchain.calculate_balances())
+   # app.run(host='0.0.0.0', port=5000)
 
-    blockchain.new_block()
-    print(blockchain.last_block)
-    print(blockchain.hash(blockchain.last_block))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
